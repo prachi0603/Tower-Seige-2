92ee18aceb7f50d760245f51064625119feb0445
prachi sharma <67380116+prachi0603@users.noreply.github.com>
2021-06-14 18:20:18 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -14,16 +14,17 @@ class Block{
     }
     display(){
       var pos= this.body.position;
-      imageMode(CENTER);
-      image(this.image, pos.x, pos.y, this.width, this.height);
-
-      if(this.body.speed > 3){
-         World.remove(world, this.body);
+      var pos= this.body.position;
+        if(this.body.speed <3){
+          imageMode(CENTER);
+          image(this.image,pos.x,pos.y,this.width, this.height);
+        }
+      else{
+        World.remove(world, this.body);
         push();
         this.visiblity = this.visiblity - 5;
         tint(255,this.visiblity);
         image(this.image, this.body.position.x, this.body.position.y,this.width, this.height);
         pop();
-      }
      }
     }
